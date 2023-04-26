8b6431cbfee5859c2e986b6df543a17230f420e6
CyPropSs <bugra.ayak@gmail.com>
2023-04-26 11:15:47 +0300

    İngilizce Kelime Ezber Uygulaması
    İngilizce Kelime Ezber Uygulaması

---
+++ Kelimeler/Kelimeler/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Kelimeler
+{
+    partial class Kelime
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kelime 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(359, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anlamı 2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kelime Getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cevapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Tabloya Ekle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(158, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Tablodan Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "İd  3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Doğru Cevap Sayısı: 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(359, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tablodaki Veri Sayısı 5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(248, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Basamaklar 7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(248, 62);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Kelime Sayısı 8";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(117, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(78, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "İpucu";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Kelime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 342);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(900, 400);
+            this.Name = "Kelime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bugi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Kelimeler/Kelimeler/Form1.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kelimeler
+{
+    public partial class Kelime : Form
+    {
+     
+      
+        public Kelime()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        int sayac = 0;
+        string id;
+        string a;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Text = "Doğru Cevap : 0";
+            {
+                con = new SqlConnection("server=.;Initial Catalog =Kelimeler;Integrated Security=SSPI");
+                cmd = new SqlCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "select top 1 id from Kelimeler.dbo.Sayfa1$ order by id desc";
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    label5.Text = "";
+                    label5.Text += dr["id"];
+
+                }
+                dr.Close();
+
+                con.Close();
+            }
+             a = Interaction.InputBox("Lütfen Bugünlük Hedefinizi giriniz", "Tekrardan Hoşgeldiniz", "", 700, 381);
+            if (a=="")
+            {
+                goto sonnas;
+            }
+          aa = Convert.ToInt32(a);
+            MessageBox.Show("Hedefiniz "+a+" Olarak Ayarlanmıştır.");
+            label4.Text = "Kalan Kelime :" + aa;
+            sonnas:;
+        }
+
+        private int aa;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection("server=.;Initial Catalog =Kelimeler;Integrated Security=SSPI");
+            cmd = new SqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select top 1 id,kelime,anlamı from [Kelimeler].[dbo].[Sayfa1$] order by newid()";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label1.Text += dr["kelime"];
+                label2.Text += dr["anlamı"];
+                label3.Text += dr["id"];
+            }
+            dr.Close();
+            con.Close();
+            label7.Text = "";
+
+            string[] kelimesayisi= label2.Text.Split(' ');
+            label8.Text ="Kelime Sayısı: "+ kelimesayisi.Length.ToString();
+            int basamak=label2.Text.Length;
+
+            for (int i = 0; i < basamak; i++)
+            {
+                label7.Text += "_ ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var hilekodu = "123";
+            if (textBox1.Text == label2.Text || textBox1.Text == hilekodu)
+            {
+                if (a=="")
+                {
+                    sayac++;
+                    label4.Text = "Doğru Cevap :" + sayac;
+                }
+                else
+                {
+                    sayac -= 1;
+                    label4.Text = "Kalan Kelime :" + (aa + sayac);
+                    if ((aa + sayac) == 0)
+                    {
+
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Yazdığınız Cevap Yanlıştır. Doğrusu :" + label2.Text);
+
+
+            }
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                con = new SqlConnection("server=.;Initial Catalog =Kelimeler;Integrated Security=SSPI");
+                cmd = new SqlCommand();
+                con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "select top 1 id from Kelimeler.dbo.Sayfa1$ order by id desc";
+
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    label5.Text = "";
+                    label5.Text += dr["id"];
+
+                }
+                dr.Close();
+
+                con.Close();
+            }
+            int sayi = int.Parse(label5.Text);
+            sayi++;
+            label5.Text = sayi.ToString();
+            string sorgu = "insert into Kelimeler.dbo.Sayfa1$ (id,kelime,anlamı) values (@id,@kelime,@anlamı)";
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@id", sayi);
+            cmd.Parameters.AddWithValue("@kelime", label1.Text);
+            cmd.Parameters.AddWithValue("@anlamı", label2.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayi = int.Parse(label3.Text);
+            string sorgu = "delete from Kelimeler.dbo.Sayfa1$ where id=@id";
+            cmd = new SqlCommand(sorgu, con);
+            cmd.Parameters.AddWithValue("@id", sayi);
+            cmd.Parameters.AddWithValue("@kelime", label1.Text);
+            cmd.Parameters.AddWithValue("@anlamı", label2.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // kelimenin uzunluğunu alıp rastgele bir sayı elde etmek için yazıldı.
+
+                  Random rastgele = new Random();
+                  int LengthAsInteger = label2.Text.Length;
+            // dongu sayinin 0dan küçük olmaması için kuruldu (eğer 0 olursa almak istediğim değer 1den küçük oluyor)
+
+            dongu:
+            int _randomNumber = rastgele.Next(LengthAsInteger + 1);
+            if (_randomNumber <= 0)
+            {
+                goto dongu;
+            }
+            // aldığım rastgele sayının önce gerçek harf değerini öğrenip sonrasında gizlenmiş metindeki yerine yazıyorum
+            var _theHint = label2.Text.Substring(_randomNumber - 1, 1);
+            string _newWord = chreplace(label7.Text, (_randomNumber * 2) - 2, _theHint);
+            if (label7.Text==_newWord)
+            {
+                goto dongu;
+            }
+            label7.Text = _newWord;
+        }
+        //gizli metindeki indisi değiştirmek için gerekli method
+        static string chreplace(string _words, int _indis, string _newLetter)
+        {
+            _words = _words.Remove(_indis, 1);
+            return _words.Insert(_indis, _newLetter);
+        }
+    }
+}
